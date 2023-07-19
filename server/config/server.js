158bcfea2f2531b79dbe03e99304ebda9c576c3f
Sanyam Sharma <84903641+Sanyam17-beep@users.noreply.github.com>
2023-07-19 23:14:30 +0530

    Update server.js

--- server/config/server.js
+++ server/config/server.js
@@ -1,8 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env("RENDER_EXTERNAL_URL"),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  dirs: {
+    public: "/data/public"
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
