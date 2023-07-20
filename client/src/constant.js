703682c8b982dea2601de5cc09eafe9711adbfb5
ishn123 <62070580+ishn123@users.noreply.github.com>
2023-07-20 20:49:35 +0530

    Update constant.js

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "http://localhost:1337/api";
+export const API = "http://0.0.0.0:1337/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
