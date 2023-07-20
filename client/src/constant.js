3dbe1e8615188f85ae797e4546d1cef33d40f31d
ishn123 <62070580+ishn123@users.noreply.github.com>
2023-07-20 21:03:15 +0530

    Update constant.js

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,4 +1,4 @@
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = REACT_APP_SERVER_BASE_URL + "api";
+export const API = "https://ecommerce-teal-delta.vercel.app/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
